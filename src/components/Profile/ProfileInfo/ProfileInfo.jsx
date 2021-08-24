dbabb5bec30546a0779dfe86e2b06a9709087873
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-08-24 16:16:14 +0600

    6th commit. Created Profile.jsx Fixed Profile jsx

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './ProfileInfo.module.css'
+const ProfileInfo = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img
+                    src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'></img>
+            </div>
+            <div className={s.descriptionBlock}>
+                ava + description
+            </div>
+        </div>
+    )
+}
+
+    export default ProfileInfo
